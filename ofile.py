6ebec287320c30ba73d18d3733810ad14c877ffb
Amit Das <amdas@redhat.com>
2020-08-14 00:03:11 +1000

    File operation script

---
+++ ofile.py
@@ -0,0 +1,70 @@
+#
+# Date Aug 2020
+# File handling
+#
+
+import os
+
+# Filename input function
+
+def filenm():
+	global tmpv
+	# tmpv = input("Enter FileName: ")     # py format in 3.6 
+	tmpv =  raw_input("Enter FileName: ")  # PY FORMAT IN 2.7 
+		
+# reading txt file
+
+def rdin():
+	f = open(tmpv, "w+")
+	for ix in range(10):
+		f.write("Intitial line:" + str(ix) + "\n")
+	f.close()
+
+
+#f1 = open("tmpv.txt","r")
+#f2 = open("tmpv.txt","r")
+
+#print(f2.read(10))
+#print(f3.readline())
+
+# writing text file
+
+def ltin():
+	i = 0
+	f1 = open(tmpv, "r")
+	for line in f1:
+		print("In loop: " + str(i) + " " + line)
+		#print(line)
+		i += 1
+	f1.close()
+
+# appending lines
+
+def apndin():
+	f2 = open(tmpv,"a+")
+	for apnd in range(2):
+		f2.write("Appending lines: " + str(apnd) + "\n")
+	f2.close()
+
+# deleting file
+
+def delin():
+	del_tmpv = raw_input("Re-enter filename to delete: ")
+	if os.path.exists(del_tmpv):
+		if del_tmpv == tmpv:
+			os.remove(del_tmpv)
+			print("File deleted")
+		else:
+			print("Created file " + tmpv  + " and delete file " + del_tmpv + " are not matching.")
+	else:
+		print("File " + del_tmpv + " doesnot exist")
+
+filenm()	
+rdin()
+apndin()
+ltin()
+delin()
+
+#f2.close()
+#f3.close()
+#f.close()
