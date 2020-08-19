b0ea2e22d06c2ef985deedd9998884b4ae06384a
Amit Das <amdas@redhat.com>
2020-08-19 22:03:00 +1000

    Updating type and shape

--- chknumpy.py
+++ chknumpy.py
@@ -1,4 +1,16 @@
+#
+# Date Aug 2020
+# importing and defining numpy package
+#
+
+import sys
 import numpy as np
+
+# 0-D, 1-D, 2-D, 3-D arrays defined
+arr0d = np.array(99)
+arr1d = np.array([99, 199, 299, 399, 499, 599])
+arr2d = np.array([[99, 199, 299], [100, 200, 300]])
+arr3d = np.array([[[1000, 2000, 3000], [999, 2999, 3999]], [[001, 002, 003], [599, 688, 777]]])
 
 print("Numpy version : " + np.__version__)
 arr = np.array([1, 2, 3])
@@ -18,12 +30,6 @@ print("\n====Creating Dimension array===\n")
 #arr2d = np.array([[100, 200, 300], [99, 199, 299]])
 #print(arr2d)
 
-# 0-D, 1-D, 2-D, 3-D arrays defined
-arr0d = np.array(99)
-arr1d = np.array([99, 199, 299, 399, 499, 599])
-arr2d = np.array([[99, 199, 299], [100, 200, 300]])
-arr3d = np.array([[[1000, 2000, 3000], [999, 2999, 3999]], [[001, 002, 003], [599, 688, 777]]])
-
 # ndim gives array dimension value
 print(str(arr0d) + " is " + str(arr0d.ndim) + " Dimension\n")
 print(str(arr1d) + " is " + str(arr1d.ndim) + " Dimension\n")
@@ -33,7 +39,7 @@ print(str(arr3d) + " is " + str(arr3d.ndim) + " Dimension\n")
 # Fetching values from the arrays dimension
 print("===Retrieving arrays value===")
 print("\n1-D second value : " + str(arr1d[1]))
-print("\n1-D second and third value : " + str(arr1d[1]) + " " + str(arr1d[2]))
+print('\n>>>>>>1-D second and third value : {0} {1}'.format(str(arr1d[1]), str(arr1d[2])))
 
 # summation from the array
 x = arr1d[1] + arr1d[2]
@@ -59,6 +65,28 @@ print("\nAll first 3 elements in 1-D : " + str(arr1d[:3]))
 
 # 3-D slice
 print("\n3-D slice : " + str(arr3d[1:2, 0:1, 1:2]))
+
+# Data Type
+arrtyp1 = np.array([99, 98, 97, 96])
+arrtyp2 = np.array(['Xxx', 'Yyy', 'Zzz'])
+print("\nData Type for array :" + str(arrtyp1) + " is " + str(arrtyp1.dtype))
+print("\nData Type for array :" + str(arrtyp2) + " is " + str(arrtyp2.dtype))
+
+# String array and conversion data type
+arrtyp3 = np.array(([[100, 200, 300]], [[1, 2, 3]]), dtype='S')
+print('\nData Type for array : {0} is {1}'.format(str(arrtyp3), str(arrtyp3.dtype)))
+      #+ str(arrtyp3) + " is " + str(arrtyp3.dtype))
+
+arrtyp4 = arrtyp3.astype(int)
+print('\nData Type conversion str to int :{0} is {1}'.format(str(arrtyp4), str(arrtyp4.dtype)))
+
+arrcp = arrtyp3.copy()
+# arrvw = arrytyp1.view()
+arrcp[0, 0] = 6
+print('\nCopy of array {0} and replaced element to {1}'.format(str(arrtyp3), str(arrcp)))
+# copy returns none
+print('\nBase of original array : ' + str(arrtyp3.base))
+print('\nBase and shape of copy array : {0} {1}'.format(str(arrcp.base), str(arrcp.shape)))
 
 # EOF
 
