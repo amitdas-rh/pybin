444afb6d37dd51a1724e999c4426d6eed6960030
Amit Das <amdas@redhat.com>
2020-08-20 23:53:07 +1000

    Iterating odd,even numbers

---
+++ IterNum.py
@@ -0,0 +1,65 @@
+#
+# Date Aug 2020
+# Iterating using numpy
+#
+
+import numpy as np
+
+# version
+print("Numpy version : " + np.__version__)
+
+# 0-D, 1-D, 2-D, 3-D arrays defined
+arr0d = np.array(99)
+arr1d = np.array([99, 199, 299, 399, 499, 599])
+arr2d = np.array([[99, 199, 299], [100, 200, 300]])
+arr3d = np.array([[[1000, 2000, 3000], [999, 2999, 3999]], [[001, 002, 003], [599, 688, 777]]])
+
+# Runtime 1D list creation
+# Then convert list to array
+
+# Create empty list
+lx = []
+
+# User input for list
+def usrentr():
+    usrv = int(raw_input("Enter list size : "))
+    for x in range(0, usrv):
+        val = int(raw_input("~> "))
+        lx.append(val)
+    print("Done")
+    print(lx)
+
+# converting list to array
+def aryconv():
+    """
+
+    :rtype: object
+    """
+    ary = np.array(lx)
+    print("\nConverted to array")
+    print newary
+    return ary
+
+# Checking odd even
+def odevn(ary):
+    odc = 0  # type: int
+    evnc = 0
+    for nbr in ary:
+        if (nbr % 2) == 0: # check even
+            print("~> {0} is even".format(nbr))
+            evnc += 1
+        else:
+            print("~> {0} is odd".format(nbr))
+            odc += 1
+    print("\n Total {0} even number and {1} odd number in the array".format(evnc, odc))
+
+# Call user entry
+usrentr()
+
+# Convert and return array
+newary = aryconv()
+
+# Check odd even in array
+odevn(newary)
+
+#EOF
